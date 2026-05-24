91f8ea9ae6f59f27f98a47de73cfdf08be09618d
Dossanbayev Roma <rahmandosanbayev@gmail.com>
2026-05-24 12:07:42 +0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Rakhat Dossanbayev on 24.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
