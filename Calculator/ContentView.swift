67e71c787e107ff99907a724ed338abc4f3b2890
Dossanbayev Roma <rahmandosanbayev@gmail.com>
2026-05-25 22:23:02 +0500

    first commit

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -2,21 +2,62 @@
 //  ContentView.swift
 //  Calculator
 //
+
+
 //  Created by Rakhat Dossanbayev on 24.05.2026.
 //
 
 import SwiftUI
 
+struct Person {
+  let name: String
+  var age: Int
+  func greet() -> String {
+    return "Привет, меня зовут \(name)!"
+  }
+}
+
+
 struct ContentView: View {
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+            
+            
+            Image(systemName:"heart.fill")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.red)
+                .padding(25)
+            
+            Text("Title")
+            Text("Description description")
+            
+            Button(action: {
+                        print("Нажал кнопку удаления")
+                    }) {
+                        Label(
+                            title: {
+                                Text("Удалить")
+                                    .fontWeight(.semibold)
+                                    .font(.title)
+                            }, icon: {
+                                Image(systemName: "trash")
+                                    .font(.title)
+                            } )
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.red)
+                        .cornerRadius(40)
+                    }
+            
         }
-        .padding()
+        
     }
+
 }
 
 #Preview {
