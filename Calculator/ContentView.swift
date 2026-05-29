7b4e9c85458bacb1b575e9487e13dc9cf06d57d5
Dossanbayev Roma <rahmandosanbayev@gmail.com>
2026-05-29 09:57:29 +0500

    I added some buttons to looking like calculator

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -15,13 +15,43 @@ struct ContentView: View {
     var body: some View {
         VStack {
             
-            //            Image(systemName: "globe")
-            //                .imageScale(.large)
-            //                .foregroundStyle(.tint)
             
-            // Вынеси кнопку в отдельный View или используй HStack/VStack
-            HStack(spacing: 16) {
-                ForEach(["1", "2", "3",], id: \.self) { num in
+             HStack(spacing: 20) {
+                 ForEach(["1", "2", "3"], id: \.self) { num in
+                     Button {
+                         print("Нажал \(num)")
+                     } label: {
+                         Text(num)
+                             .fontWeight(.semibold)
+                             .font(.title)
+                             .foregroundColor(.white)
+                             .frame(width: 60, height: 60)
+                             .background(Color.red)
+                             .cornerRadius(10)
+                     }
+                 }
+             }
+
+            
+             HStack(spacing: 20) {
+                 ForEach(["4", "5", "6"], id: \.self) { num in
+                     Button {
+                         print("Нажал \(num)")
+                     } label: {
+                         Text(num)
+                             .fontWeight(.semibold)
+                             .font(.title)
+                             .foregroundColor(.white)
+                             .frame(width: 60, height: 60)
+                             .background(Color.red)
+                             .cornerRadius(10)
+                     }
+                 }
+             }
+            
+            
+            HStack(spacing: 20) {
+                ForEach(["7", "8", "9"], id: \.self) { num in
                     Button {
                         print("Нажал \(num)")
                     } label: {
@@ -35,16 +65,43 @@ struct ContentView: View {
                     }
                 }
             }
-            
-            
-            
-            
+
+
+
+
+            HStack(spacing: 30)
+            {
+                 Button {
+                     print("Удалить")
+                 } label: {
+                     Text("Удалить")
+                         .fontWeight(.semibold)
+                         .foregroundColor(.white)
+                         .frame(maxWidth: .infinity)
+                         .padding()
+                         .background(Color.red)
+                         .cornerRadius(12)
+                 }
+                
+                 Button {} label: {
+                     Text("Отмена")
+                         .fontWeight(.semibold)
+                         .foregroundColor(.white)
+                         .frame(maxWidth: .infinity)
+                         .padding()
+                         .background(Color.gray)
+                         .cornerRadius(12)
+                 }
+             }
+             .padding(.horizontal)
+         }
+         .padding(.top)
             
         }
         
     }
 
-}
+
 
 #Preview {
     ContentView()
